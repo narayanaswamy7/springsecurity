4e9eefe2454d1361783615c5b429840e1f4c99d5
narayana.swamy <nswamy751@gmail.com>
2022-01-10 17:52:29 +0530

    spring security test app

---
+++ src/main/java/com/ns/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.ns.springsecurity.security;
+
+import static com.ns.springsecurity.security.ApplicationUserPermission.*;
+import static com.ns.springsecurity.security.ApplicationUserRole.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		super();
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+		.antMatchers("/api/**").hasRole(STUDENT.name())
+		.antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.PUT, "management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.GET, "management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails annaSmithUser = User
+				.builder()
+				.username("annasmith")
+				.password(passwordEncoder.encode("password"))
+				.roles(STUDENT.name()) // ROLE_STUDENT
+				.build();
+		
+		UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name()) // ROLE_ADMIN
+                .build();
+		
+		UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .build();
+		
+		return new InMemoryUserDetailsManager(
+				annaSmithUser,
+				lindaUser,
+				tomUser
+				);
+	}
+
+}
